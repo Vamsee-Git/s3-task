8bacaa1c3dcc6c563331dbcbb5a4467efaa76832
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-03 18:55:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create an S3 bucket.
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "new-s3-bucket-vamsee"
+  acl    = "private"
+}
+
+# Enable versioning for the bucket
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption for the bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Upload a sample file test.txt to the S3 bucket
+resource "aws_s3_bucket_object" "test_file" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  key    = "test.txt"
+  source = "test.txt"
+  acl    = "private"
+}
+
+# Make the uploaded file publicly accessible using an ACL
+resource "aws_s3_bucket_object_acl" "public_read_acl" {
+  bucket = aws_s3_bucket_object.test_file.bucket
+  key    = aws_s3_bucket_object.test_file.key
+  acl    = "public-read"
+}
