0fea5dc198131c45f25df97e379f2b1ac1a552de
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 11:42:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,4 +33,29 @@ resource "aws_s3_object" "test_file" {
   key    = "test.txt"
   source = "test.txt"
 }
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "arn:aws:s3:::new-s3-bucket-vamsee/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
